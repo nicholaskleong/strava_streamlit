79ceeb5a116ded1da2caa00f8484b84a5a779b74
Nicholas Leong <nicholas.leong@ecodev.vic.gov.au>
2025-01-08 09:36:37 +1100

    update default activities

--- summary.py
+++ summary.py
@@ -17,8 +17,9 @@ if 'access_token' not in st.session_state:
 access_token = st.session_state['access_token']
 activities = init_data(access_token)
 act_options = activities.sport_type.unique()
-default_type = activities.groupby('sport_type').distance.sum().sort_values().index[-1]
-act_list = st.multiselect('Activities',act_options, default = [default_type])
+default_types = [activities.groupby('sport_type').distance.sum().sort_values().index[-1]]
+default_types =['Run','Trail Run']
+act_list = st.multiselect('Activities',act_options, default = default_types)
 filtered_activities = filter_activities(activities,act_list)
 st.session_state['filtered_activities'] = filtered_activities
 #Metrics
